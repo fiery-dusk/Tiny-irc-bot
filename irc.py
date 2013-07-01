376499bc91f65dbf2caf7312c868a20bcafda1a6
fiery dusk <cameron@fierydusk.net>
2013-07-01 21:56:59 +1200

    initial commit

---
+++ irc.py
@@ -0,0 +1,37 @@
+import socket
+import sys
+from time import sleep
+
+user='lurker'
+channel='#bots'
+nick='el_fiery_dusko'
+
+irc=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+print('connecting to server')
+
+irc.connect(('irc.dongcorp.org', 6667))
+irc.send('NICK '+nick+'\n')
+irc.send('USER '+user+' '+user+' '+user+' :notevenalurkbot')
+irc.send('JOIN '+channel+'\n')
+
+try:
+	while True:
+		text=irc.recv(4096)
+		print (text)
+		if text.find(':lel') !=-1:
+			x=1
+			while x<=10:
+				irc.send('PRIVMSG '+channel+' :lel \r\n')
+				sleep(.1)
+				x=x+1
+		if text.find('PING') !=-1:
+			irc.send('PONG %s\r\n' % text[1])
+		
+		if text.find('376') !=-1:
+			irc.send('JOIN '+channel+'\n')
+except(KeyboardInterrupt):
+	irc.close()
+	print('\n\nHave a nice day :)')
+	sleep(1)
+
